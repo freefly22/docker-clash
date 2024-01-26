23b920262422fcfb7403de39d3728bedccddf9f5
freefly22 <73732106+freefly22@users.noreply.github.com>
2024-01-26 17:49:40 +0800

    Update dl-clash-conf.bash

--- files/dl-clash-conf.bash
+++ files/dl-clash-conf.bash
@@ -13,14 +13,14 @@ then
 fi
 
 # 写入 API端口
-if [[ ! -z "$EXTERNAL_BIND" && ! -z "$EXTERNAL_PORT" ]]
-then
-  echo "external-controller: $EXTERNAL_BIND:$EXTERNAL_PORT" >> $ConfFile
-fi
+#if [[ ! -z "$EXTERNAL_BIND" && ! -z "$EXTERNAL_PORT" ]]
+#then
+#  echo "external-controller: $EXTERNAL_BIND:$EXTERNAL_PORT" >> $ConfFile
+#fi
 # 鉴权信息
-if [[ ! -z "$EXTERNAL_SECRET" ]]
-then
-  echo "secret: \"$EXTERNAL_SECRET\"" >> $ConfFile
-fi
+#if [[ ! -z "$EXTERNAL_SECRET" ]]
+#then
+#  echo "secret: \"$EXTERNAL_SECRET\"" >> $ConfFile
+#fi
 # 必须开启局域网连接, 否则外部无法连接
-echo "allow-lan: true" >> $ConfFile
+# echo "allow-lan: true" >> $ConfFile
